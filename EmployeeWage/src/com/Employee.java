05d25912b742b82c802cf4971808745ab6868b2d
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-14 23:12:46 +0530

    added monthly wage calculation

--- EmployeeWage/src/com/Employee.java
+++ EmployeeWage/src/com/Employee.java
@@ -5,38 +5,44 @@ public class Employee {
 	private int WAGE_PER_HOUR = 20;
 	private int FULL_TIME_HOUR = 8;
 	private int PART_TIME_HOUR = 4;
+	private int WORKING_DAYS_PER_MONTH = 20;
 	private int salary = 0;
-	private int isPresent = (int)Math.floor(Math.random() * 10) % 2;
-	private int isPartTime = (int)Math.floor(Math.random() * 10) % 2;
 	
-	void employeeAttendance() {
-		
-		switch(isPresent) {	
-		case 0:
-			System.out.println("Employee Absent today");
-			break;
-		case 1:
-			System.out.println("Employee Present today");
-			break;
-		}
+	
+	int getWorkingDaysPerMonth(){
+		return WORKING_DAYS_PER_MONTH;
 	}
 	
-	void employeeWage() {
-		
+	int getEmployeeSalary() {
+		return salary;
+	}
+	
+	int employeeAttendance() {
+		int isPresent = (int)Math.floor(Math.random() * 100) % 3;
+		return isPresent;
+	}
+	
+	int employeeDayWage(int isPresent) {
+		int dayWage = 0;
 		switch(isPresent) {	
-		case 0:
+		case 2:
+			System.out.println("Present and Working full time.");
+			dayWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+			salary = salary + ( WAGE_PER_HOUR * FULL_TIME_HOUR );
 			break;
 		case 1:
-			switch(isPartTime) {
-			case 0:
-				salary = WAGE_PER_HOUR * FULL_TIME_HOUR;
-				break;
-			case 2:
-				salary = WAGE_PER_HOUR * PART_TIME_HOUR;
-				break;
-			}			
+			System.out.println("Present and Working part time.");
+			dayWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+			salary = salary + ( WAGE_PER_HOUR * PART_TIME_HOUR );
+			break;
+		case 0:
+			System.out.println("Absent.");
+			break;
 		}
-		System.out.println("Employee wage is " + salary);
+		return dayWage;
+	}
+	void printMonthWage(){
+		System.out.println("Employee salary for this month is " + salary);
 	}
 
 }
--- EmployeeWage/src/com/Main.java
+++ EmployeeWage/src/com/Main.java
@@ -3,8 +3,13 @@ package com;
 public class Main {
 	public static void main(String[] args) {
 		Employee e1 = new Employee();
-		e1.employeeAttendance();
-		e1.employeeWage();
+		int attendance;
+		for(int i = 1; i <= e1.getWorkingDaysPerMonth(); i++) {
+			attendance = e1.employeeAttendance();
+			System.out.println("Day: " + i);			
+			System.out.println("Day Wage: " + e1.employeeDayWage(attendance) + "\n");
+		}
+		e1.printMonthWage();
 	}
 	
 }
