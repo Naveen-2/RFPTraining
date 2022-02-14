6d768bf341facd476f9781c50ffbaf955e1733c8
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-14 23:33:54 +0530

    Updated maximum working hours/days in a month.

--- EmployeeWage/src/com/Employee.java
+++ EmployeeWage/src/com/Employee.java
@@ -6,15 +6,25 @@ public class Employee {
 	private int FULL_TIME_HOUR = 8;
 	private int PART_TIME_HOUR = 4;
 	private int WORKING_DAYS_PER_MONTH = 20;
+	private int TOTAL_WORKING_HOURS = 100;
 	private int salary = 0;
+	private int workedHours = 0;
 	
 	
 	int getWorkingDaysPerMonth(){
 		return WORKING_DAYS_PER_MONTH;
 	}
 	
+	int getTotalWorkingHours() {
+		return TOTAL_WORKING_HOURS;
+	}
+	
 	int getEmployeeSalary() {
 		return salary;
+	}
+	
+	int getWorkedHours() {
+		return workedHours;
 	}
 	
 	int employeeAttendance() {
@@ -26,23 +36,27 @@ public class Employee {
 		int dayWage = 0;
 		switch(isPresent) {	
 		case 2:
-			System.out.println("Present and Working full time.");
+			System.out.println(" - Present and Working full time.");
 			dayWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
 			salary = salary + ( WAGE_PER_HOUR * FULL_TIME_HOUR );
+			workedHours = workedHours + FULL_TIME_HOUR;
 			break;
 		case 1:
-			System.out.println("Present and Working part time.");
+			System.out.println(" - Present and Working part time.");
 			dayWage = WAGE_PER_HOUR * PART_TIME_HOUR;
 			salary = salary + ( WAGE_PER_HOUR * PART_TIME_HOUR );
+			workedHours = workedHours + PART_TIME_HOUR;
 			break;
 		case 0:
-			System.out.println("Absent.");
+			System.out.println(" - Absent.");
 			break;
 		}
+		System.out.println("Worked " + workedHours + " hours so far.");
 		return dayWage;
 	}
+	
 	void printMonthWage(){
-		System.out.println("Employee salary for this month is " + salary);
+		System.out.println("Employee worked " + workedHours + " hours and earned Rs. " + salary + " this month.");
 	}
 
 }
--- EmployeeWage/src/com/Main.java
+++ EmployeeWage/src/com/Main.java
@@ -4,10 +4,16 @@ public class Main {
 	public static void main(String[] args) {
 		Employee e1 = new Employee();
 		int attendance;
+		//works till maximum working days 20.
 		for(int i = 1; i <= e1.getWorkingDaysPerMonth(); i++) {
-			attendance = e1.employeeAttendance();
-			System.out.println("Day: " + i);			
-			System.out.println("Day Wage: " + e1.employeeDayWage(attendance) + "\n");
+			//works till working hours cross 100.
+			if(e1.getWorkedHours() <= e1.getTotalWorkingHours()) {
+				attendance = e1.employeeAttendance();
+				System.out.print("Day: " + i);			
+				System.out.println("Day Wage: Rs. " + e1.employeeDayWage(attendance) + "\n");
+			} else {
+				break;
+			}
 		}
 		e1.printMonthWage();
 	}
