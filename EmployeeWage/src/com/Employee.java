91a3cd7b574f7fbc816423ca1515cbd9d7d8f6b3
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-14 20:16:23 +0530

    Added PartTime employee feature
    Now employees can wither be part time who work 4hrs or full time who work 8hrs

--- EmployeeWage/src/com/Employee.java
+++ EmployeeWage/src/com/Employee.java
@@ -2,23 +2,28 @@ package com;
 
 public class Employee {
 	
-	private int isPresent;
 	private int WAGE_PER_HOUR = 20;
-	private int FULL_DAY_HOUR = 8;
+	private int FULL_TIME_HOUR = 8;
+	private int PART_TIME_HOUR = 4;
 	private int salary = 0;
+	private int isPresent = (int)Math.floor(Math.random() * 10) % 2;
+	private int isPartTime = (int)Math.floor(Math.random() * 10) % 2;
+	
 	void employeeAttendance() {
-		isPresent = (int)Math.floor(Math.random() * 10) % 2;
-		if(isPresent == 0) { 
+		
+		if(isPresent == 1) 
 			System.out.println("Employee Present today");
-			employeeWage();
-		} else
+		else
 			System.out.println("Employee Absent today");
+		
 	}
 	
 	void employeeWage() {
-		if(isPresent == 0)
-			salary = salary + (WAGE_PER_HOUR * FULL_DAY_HOUR);
-		System.out.println("Employee salary is " + salary);
+		
+		if(isPresent == 1) 
+			salary = salary + (WAGE_PER_HOUR * ((isPartTime == 1) ? PART_TIME_HOUR : FULL_TIME_HOUR ));
+		System.out.println("Employee wage is " + salary);
+	
 	}
 
 }
--- EmployeeWage/src/com/Main.java
+++ EmployeeWage/src/com/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Employee e1 = new Employee();
 		e1.employeeAttendance();
+		e1.employeeWage();
 	}
 	
 }
