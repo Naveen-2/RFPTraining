f666a809314d2731098f0e45cb884f46afe46aa7
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-14 19:59:17 +0530

    Calculated EMployee wage

--- EmployeeWage/src/com/Employee.java
+++ EmployeeWage/src/com/Employee.java
@@ -1,12 +1,24 @@
 package com;
 
 public class Employee {
+	
+	private int isPresent;
+	private int WAGE_PER_HOUR = 20;
+	private int FULL_DAY_HOUR = 8;
+	private int salary = 0;
 	void employeeAttendance() {
-		int isPresent;
-		isPresent = (int)Math.floor(Math.random() * 10);
-		if(isPresent % 2 == 0) 
+		isPresent = (int)Math.floor(Math.random() * 10) % 2;
+		if(isPresent == 0) { 
 			System.out.println("Employee Present today");
-		else
+			employeeWage();
+		} else
 			System.out.println("Employee Absent today");
 	}
+	
+	void employeeWage() {
+		if(isPresent == 0)
+			salary = salary + (WAGE_PER_HOUR * FULL_DAY_HOUR);
+		System.out.println("Employee salary is " + salary);
+	}
+
 }
