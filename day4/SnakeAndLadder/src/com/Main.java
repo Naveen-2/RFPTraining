bab2ddcf8961c2e608831faaa9b59a537b49caf8
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-15 18:16:17 +0530

    SnakeAndLadder program
    Program runnable for 2 players now.

--- day4/SnakeAndLadder/src/com/Main.java
+++ day4/SnakeAndLadder/src/com/Main.java
@@ -3,9 +3,33 @@ package com;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SnakeAndLadder player1 = new SnakeAndLadder();
-		System.out.println("Player started playing at position: " + player1.getPlayerPosition());
+		
+		SnakeAndLadder player1 = new SnakeAndLadder("Player1");
+		SnakeAndLadder player2 = new SnakeAndLadder("Player2");
+		
+		String player1Name = player1.getPlayerName();
+		String player2Name = player2.getPlayerName();
+		
+		System.out.println(player1Name + " started playing at position: " + player1.getPlayerPosition() + ".\n");
+		System.out.println(player1Name + " started playing at position: " + player2.getPlayerPosition() + ".\n");
+		
+		
+		while ( true ) {	
+			
+			System.out.println(player1Name + "'s turn..");
+			player1.rollDice();
+			System.out.println(player1Name + " reached position " + player1.getPlayerPosition() + ".\n");
+			if(player1.checkWinning()) 
+				break;
+			
+			System.out.println(player2Name + "'s turn..");
+			player2.rollDice();
+			System.out.println(player2Name + " reached position " + player2.getPlayerPosition() + ".\n");
+			if(player2.checkWinning()) 
+				break;
+			
+		}
+		
 	}
 
 }
--- day4/SnakeAndLadder/src/com/SnakeAndLadder.java
+++ day4/SnakeAndLadder/src/com/SnakeAndLadder.java
@@ -1,8 +1,81 @@
 package com;
 
 public class SnakeAndLadder {
+	
+	private int WINNING_POSITION = 100;
 	private int playerPosition = 0;
+	private int numberOfDieRolls = 0;
+	private int newPosition = 0;
+	private String playerName = "";
+	
 	int getPlayerPosition() {
 		return playerPosition;
 	}
+
+	int getWinningPostion() {
+		return WINNING_POSITION;
+	}
+	
+	int getNumberOfDieRolls() {
+		return numberOfDieRolls;
+	}
+	
+	SnakeAndLadder(String name){
+		playerName = name;
+	}
+	
+	String getPlayerName() {
+		return playerName;
+	}
+	
+	int rollDice() {
+		int number = (int) ((Math.random() * 100) % 6) + 1;
+		numberOfDieRolls += 1;
+		System.out.println("Dice Value: " + number);
+		newPosition = checkOptions(number);
+		return newPosition;
+	}
+	
+	
+	int checkOptions(int diceValue) {
+		int option = (int) ((Math.random() * 100) % 3) + 1;
+		switch(option) {
+		case 1:
+			//ladder
+			System.out.println(playerName + " took a ladder :)");
+			playerPosition = playerPosition + diceValue;
+			break;
+		case 2:
+			//snake
+			System.out.println(playerName + " bitten by snake :(");
+			playerPosition = playerPosition - diceValue;
+			break;
+		case 3:
+			//no move
+			System.out.println(playerName + " has no move.");
+			//playerPosition = playerPosition + 0;
+			break;
+		}
+		
+		if(playerPosition < 0)
+			playerPosition = 0;
+		else if(playerPosition > 100)
+			playerPosition = playerPosition - diceValue;
+		
+		if(option == 1)
+			rollDice();
+		
+		
+		return playerPosition;
+	}
+	
+	boolean checkWinning() {
+		if(getPlayerPosition() == 100) {
+			System.out.println("Congragulations " + getPlayerName() + "! You finished the game ;P");
+			System.out.println(getPlayerName() + " rolled the dice " + getNumberOfDieRolls() + " times to win the game.");
+			return true;
+		}
+		return false;
+	}
+	
 }
